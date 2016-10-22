c1a8eb7a3795cb02f045487932bafb16049b087f
Mike Cheong <michaelkcheong@gmail.com>
2016-10-22 02:44:19 -0700

    set up route

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,3 +1,9 @@
-module.exports = function(app) {
 
+
+module.exports = function(app) {
+	app.post("/gen_url", function(req, res){
+		// regex to clean string
+		// hash algorithm to generate key
+		// return hash
+	})
 }
