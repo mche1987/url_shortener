5ece9f6c6c98711efe02b632f579f3ca93eac006
Mike Cheong <michaelkcheong@gmail.com>
2016-10-22 22:26:18 -0700

    got redirect route to work

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,16 +1,12 @@
-// var valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
-
 var base58 = require('../helpers/base58.js')
 var config = require('./config.js')
 var Url = require('../models/urls.js')
 
 module.exports = function(app) {
 	app.post("/gen_url", function(req, res){
-		console.log(req.body.url) // long url
 		var longUrl = req.body.url;
 		var shortUrl = '';
 
-		// checking to see if already exists - this is inefficient
 		Url.findOne({long_url: longUrl}, function(err, doc){
 			if(doc){
 				shortUrl = config.webhost + base58.encode(doc._id);
@@ -22,25 +18,23 @@ module.exports = function(app) {
 
 				newUrl.save(function(err){
 					if(err) {
-						console.log(err)
-						res.send({'error': err})
-					}
-					shortUrl = config.webhost + base58.encode(newUrl._id)
+						console.log("error is: ", err)
+				}
+				shortUrl = config.webhost + base58.encode(newUrl._id)
 
-					res.send({'shortUrl': shortUrl})
+				res.send({'shortUrl': shortUrl})
 				})
 			}
 		})
 	})
 
 	app.get('/:encoded_id', function(req, res){
-		var base58Id = require.params.encoded_id
+		var base58Id = req.params.encoded_id
 		var id = base58.decode(base58Id)
 
-		// check if url already exists in db
 		Url.findOne({_id: id}, function(err, doc){
 			if(doc){
-				res.redirect(doc.long_url)
+				res.redirect('http://' + doc.long_url)
 			} else {
 				res.redirect(config.webhost)
 			}
